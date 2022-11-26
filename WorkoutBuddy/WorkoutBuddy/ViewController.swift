28d227231a3d616808372dc21eeaa62837da8688
Casey Sytsema <sytsemacj@gmail.com>
2022-11-26 14:58:07 -0500

    Adds starter Xcode files

---
+++ WorkoutBuddy/WorkoutBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutBuddy
+//
+//  Created by Casey Sytsema on 11/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
